d58a2e3ba766a832df59137e4b3f95b2efef7295
Stephen M Buchanan <buchanan.stephenm@gmail.com>
2014-04-10 23:17:50 -0700

    Adds location attrs

--- ruby/spec/sudoku_spec.rb
+++ ruby/spec/sudoku_spec.rb
@@ -6,14 +6,45 @@ describe Sudoku do
 end
 
 describe Node do
+  let(:test_node) { Node.new({ value: '1', location: 0 }) }
 
+  context "attributes" do
+    it { expect(test_node).to respond_to(:value) }
+    it { expect(test_node.value).to be_a(Integer) }
+    it { expect(test_node).to respond_to(:row_id) }
+    it { expect(test_node).to respond_to(:column_id) }
+    it { expect(test_node).to respond_to(:cell_id) }
+  end
+
+  context "methods" do
+    it { expect(test_node).to respond_to(:solved?) }
+  end
+
+  context "#solved?" do
+    it "should be true if the value is not zero" do
+      expect(test_node.solved?).to eq(true)
+    end
+
+    it "should be false if the value is zero" do
+      test_node.value = 0
+      expect(test_node.solved?).to eq(false)
+    end
+  end
+
+  context "location identifiers" do
+    it { expect(test_node.row_id).to eq(0 / 9) }
+    it { expect(test_node.column_id).to eq(0 % 9) }
+    it { expect(test_node.cell_id).to eq(0) }
+  end
 end
 
 describe Board do
-  let(:test_board) { Board.new('123') } 
+  let(:test_board) { Board.new('1'*81) }
 
   context "attributes" do
-
-    it { test_board.should respond_to(:puzzle ) }
+    it { expect(test_board).to respond_to(:puzzle ) }
+    it { expect(test_board.puzzle.length).to eq(81) }
+    it { expect(test_board.puzzle).to be_a(Array) }
+    it { expect(test_board.puzzle.first).to be_a(Node) }
   end
 end
--- ruby/sudoku.rb
+++ ruby/sudoku.rb
@@ -3,13 +3,26 @@ class Sudoku
 end
 
 class Node
+  attr_accessor :value
+  attr_reader :row_id, :column_id, :cell_id
 
+  def initialize(args)
+    @value = args[:value].to_i
+    @id = args[:location]
+    @row_id, @column_id = @id.divmod(9)
+    @cell_id = ((@row_id / 3) * 3) + (@column_id / 3)
+  end
+
+  def solved?
+    !@value.zero?
+  end
 end
 
 class Board
   attr_accessor :puzzle
 
   def initialize(puzzle)
-    @puzzle = puzzle
+    @puzzle = []
+    puzzle.split('').each_with_index { |value, index| @puzzle << Node.new({ value: value, location: index }) }
   end
 end
